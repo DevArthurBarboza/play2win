a3a804b8860921119937fddc8f8c05668ac6d6e3
DevArthurBarboza <arthur.barboza0016@gmail.com>
2023-06-25 18:56:22 -0300

    :pencil2: Fix typo for route request

--- resources/views/admin/game/create.blade.php
+++ resources/views/admin/game/create.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 flex">
-                    <form action="/dashboard/game/store" method="POST">
+                    <form action="/dashboard/games/store" method="POST">
                         @csrf
                         <label class="text-gray-200" for="name">Nome</label>
                         <input type="text" name="name" id="name">
