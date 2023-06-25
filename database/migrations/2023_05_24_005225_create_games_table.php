9deabb7e64dffeb6e4c929923a68816e35777018
DevArthurBarboza <arthur.barboza0016@gmail.com>
2023-06-25 18:31:12 -0300

    :sparkles: Implementing multiplier cash in games on databse

--- database/migrations/2023_05_24_005225_create_games_table.php
+++ database/migrations/2023_05_24_005225_create_games_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->uuid('access_code');
+            $table->float('multiplier',6,3)->default(1.0);
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
