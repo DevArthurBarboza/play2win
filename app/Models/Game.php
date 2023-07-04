5a8b85c5844cc3fab79c14ed052b56215d05804b
DevArthurBarboza <arthur.barboza0016@gmail.com>
2023-07-04 18:56:47 -0300

    :fire: Removing unused code

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 
 
 class Game extends Model
@@ -22,20 +21,5 @@ class Game extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
-    }
-
-    public function render()
-    {
-        $category = Category::find($this->category_id);
-        $type = Type::find($category->type_id);
-        if($type->code == "Roleta"){
-            return view('game.roleta',['user' => Auth::user()]);
-        }
-        if($type->code == "Crash"){
-            return view('game.crash',['user' => Auth::user()]);
-        }
-        if($type->code == "Roleta"){
-            return view('game.roleta',['user' => Auth::user()]);
-        }
     }
 }
